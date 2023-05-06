5185d570aff8cefc8a3100646f0cb5bcce02f963
Olvcpr423 <61469484+Olvcpr423@users.noreply.github.com>
2023-05-07 00:23:18 +0800

    Update index.js

--- dev/js/index.js
+++ dev/js/index.js
@@ -41,7 +41,7 @@ $(document).ready(function(){
                     'box-shadow': '0 1px 12px rgba(0, 0, 0, .08)'
                 });
                 logo.css({
-                    'background': 'url(/assets/icons/logo_' + themeStyle + '.svg) no-repeat center',
+                    'background': 'url(/avatar.jpg) no-repeat center',//原来是/assets/icons/logo_' + themeStyle + '.svg
                     'background-size': '100% 100%'
                 });
                 navText.css('color', '#666');
